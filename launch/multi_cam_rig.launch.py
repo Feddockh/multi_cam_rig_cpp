61eb1502a439fe5163bb2947327c49b3092c6b7c
Hayden Feddock <hayden4feddock@gmail.com>
2024-12-12 17:07:55 -0500

    modifiied launch file save parameter passing.

--- launch/multi_cam_rig.launch.py
+++ launch/multi_cam_rig.launch.py
@@ -7,6 +7,7 @@ import datetime
 
 def generate_launch_description():
 
+    # Define the image topics
     director_topic = '/multi_cam_rig/director'
     firefly_left_image_topic = '/flir_node/firefly_left/image_raw'
     firefly_right_image_topic = '/flir_node/firefly_right/image_raw'
@@ -14,17 +15,22 @@ def generate_launch_description():
     zed_left_image_topic = '/multi_cam_rig/zed/left_image'
     zed_right_image_topic = '/multi_cam_rig/zed/right_image'
 
-    # Create the data directory
-    workspace_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-    print(f'Workspace directory: {workspace_dir}')
-    data_dir = os.path.join(workspace_dir, 'multi_cam_rig_cpp', 'data')
-    os.makedirs(data_dir, exist_ok=True)
-    
-    # Create the save directory with the current date
-    save_dir_name = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    save_dir = os.path.join(data_dir, save_dir_name)
-    print(f'Saving images to: {save_dir}')
-    os.makedirs(save_dir, exist_ok=True)
+    # Set to True to save images to disk
+    save_images = False
+    save_dir = ''
+    if save_images:
+
+        # Create the data directory
+        workspace_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+        print(f'Workspace directory: {workspace_dir}')
+        data_dir = os.path.join(workspace_dir, 'multi_cam_rig_cpp', 'data')
+        os.makedirs(data_dir, exist_ok=True)
+        
+        # Create the save directory with the current date
+        save_dir_name = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        save_dir = os.path.join(data_dir, save_dir_name)
+        print(f'Saving images to: {save_dir}')
+        os.makedirs(save_dir, exist_ok=True)
 
     # Create the director gui node
     director_gui_node = Node(
@@ -33,7 +39,7 @@ def generate_launch_description():
         name='director_gui',
         output='screen',
         parameters=[{
-            'save_images': True,
+            'save_images': save_images,
             'save_dir': save_dir,
             'director_topic': director_topic,
             'firefly_left_image_topic': firefly_left_image_topic,
@@ -51,7 +57,7 @@ def generate_launch_description():
         name='firefly_capture_node',
         output='screen',
         parameters=[{
-            'save_images': True,
+            'save_images': save_images,
             'save_dir': save_dir,
             'director_topic': director_topic,
             'left_image_topic': firefly_left_image_topic,
@@ -66,7 +72,7 @@ def generate_launch_description():
         name='ximea_capture_node',
         output='screen',
         parameters=[{
-            'save_images': True,
+            'save_images': save_images,
             'save_dir': save_dir,
             'director_topic': director_topic,
             'ximea_image_topic': ximea_image_topic,
@@ -80,7 +86,7 @@ def generate_launch_description():
         name='zed_capture_node',
         output='screen',
         parameters=[{
-            'save_images': True,
+            'save_images': save_images,
             'save_dir': save_dir,
             'director_topic': director_topic,
             'left_image_topic': zed_left_image_topic,
